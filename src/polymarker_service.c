fc6f7bad1c9c4759c3a28e5778986a9de3bea53e
billyfish <tyrrell.simon@gmail.com>
2017-11-20 10:24:03 +0000

    Expanded service description

--- src/polymarker_service.c
+++ src/polymarker_service.c
@@ -320,7 +320,10 @@ static const char *GetPolymarkerServiceName (Service * UNUSED_PARAM (service_p))
 
 static const char *GetPolymarkerServiceDesciption (Service * UNUSED_PARAM (service_p))
 {
-	return "A service using Polymarker";
+	return "PolyMarker is an automated bioinformatics pipeline for SNP assay development which increases "
+		"the probability of generating homoeologue-specific assays for polyploid wheat. PolyMarker generates "
+		"a multiple alignment between the target SNP sequence and the IWGSC chromosome survey sequences (IWGSC, 2014 ) for each of the three wheat genomes. "
+		"It then generates a mask with informative positions which are highlighted with respect to the target genome.";
 }
 
 
