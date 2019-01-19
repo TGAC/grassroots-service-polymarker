772a46d9f3d88ed313896a322332446be8a59a05
Simon Tyrrell <tyrrell.simon@gmail.com>
2019-01-19 23:36:29 +0000

    wip on adding the prefs for Primer3

--- include/primer3_prefs.h
+++ include/primer3_prefs.h
@@ -83,7 +83,7 @@ POLYMARKER_SERVICE_LOCAL Primer3Prefs *AllocatePrimer3Prefs (const PolymarkerSer
 POLYMARKER_SERVICE_LOCAL void FreePrimer3Prefs (Primer3Prefs *prefs_p);
 
 
-POLYMARKER_SERVICE_LOCAL bool SavePrimer3Prefs (Primer3Prefs *prefs_p, const char *working_dir_s, const char *job_id_s));
+POLYMARKER_SERVICE_LOCAL bool SavePrimer3Prefs (Primer3Prefs *prefs_p, const char *working_dir_s, const char *job_id_s);
 
 
 POLYMARKER_SERVICE_LOCAL bool AddPrimer3PrefsParameters (ParameterSet *params_p, Primer3Prefs *prefs_p);
--- src/primer3_prefs.c
+++ src/primer3_prefs.c
@@ -20,45 +20,21 @@
  *      Author: billy
  */
 
+#include <stdio.h>
+#include <string.h>
+
 #include "primer3_prefs.h"
 #include "memory_allocations.h"
+#include "streams.h"
+#include "io_utils.h"
+#include "string_utils.h"
 
 
+static bool WriteKeyValuePairForString (const char *key_s, const char *value_s, FILE *out_f);
 
-typedef struct Primer3Prefs
-{
-	/*
-	 * PRIMER_PRODUCT_SIZE_RANGE (size range list; default 100-300)
-	 *
-	 * The associated values specify the lengths of the product that the user wants the
-	 * primers to create, and is a space separated list of elements of the form
-	 *
-	 * <x>-<y>
-	 *
-	 */
-	uint32 pp_product_size_range_min;
+static bool WriteKeyValuePairForUnsignedInt (const char *key_s, const uint32 value, FILE *out_f);
 
-	uint32 pp_product_size_range_max;
-
-	/*
-	 * PRIMER_MAX_SIZE (int; default 27)
-	 *
-	 * Maximum acceptable length (in bases) of a primer. Currently this parameter cannot
-	 * be larger than 35. This limit is governed by maximum oligo size for which primer3's
-	 * melting-temperature is valid.
-	 */
-	uint32 pp_max_size;
-
-	bool pp_lib_ambiguity_codes_consensus;
-
-	bool pp_liberal_base;
-
-	uint32 pp_num_return;
-
-	bool pp_explain_flag;
-
-	char *pp_thermodynamic_parameters_path_s;
-} Primer3Prefs;
+static char *GetProductSizeRangeAsString (const uint32 min_value, const uint32 max_value);
 
 
 
@@ -73,7 +49,9 @@ static NamedParameterType S_EXPLAIN_FLAG = { "Product size range min", PT_BOOLEA
 
 Primer3Prefs *AllocatePrimer3Prefs (const PolymarkerServiceData *data_p)
 {
+	Primer3Prefs *prefs_p = NULL;
 
+	return NULL;
 }
 
 
@@ -88,14 +66,71 @@ void FreePrimer3Prefs (Primer3Prefs *prefs_p)
 }
 
 
-bool SavePrimer3Prefs (Primer3Prefs *prefs_p, const char *working_dir_s, const char *job_id_s)
+bool SavePrimer3Prefs (Primer3Prefs *prefs_p, const char *working_dir_s, const char *job_id_s, const char *primer_config_path_s)
 {
 	bool success_flag = false;
 	char *filename_s = MakeFilename (working_dir_s, job_id_s);
 
 	if (filename_s)
 		{
+/*
+      :primer_product_size_range => "50-150" ,
+      :primer_max_size => 25 ,
+      :primer_lib_ambiguity_codes_consensus => 1,
+      :primer_liberal_base => 1,
+      :primer_num_return=>5,
+      :primer_explain_flag => 1,
+      :primer_thermodynamic_parameters_path
+ */
+			FILE *out_f = fopen (filename_s, "w");
 
+			if (out_f)
+				{
+					char *range_s = GetProductSizeRangeAsString (prefs_p -> pp_product_size_range_min, prefs_p -> pp_product_size_range_max);
+
+					if (range_s)
+						{
+							if (WriteKeyValuePairForString ("primer_product_size_range", range_s, out_f))
+								{
+									if (WriteKeyValuePairForUnsignedInt ("primer_max_size", prefs_p -> pp_max_size, out_f))
+										{
+											if (WriteKeyValuePairForUnsignedInt ("primer_lib_ambiguity_codes_consensus", prefs_p -> pp_lib_ambiguity_codes_consensus ? 1 : 0, out_f))
+												{
+													if (WriteKeyValuePairForUnsignedInt ("primer_liberal_base", prefs_p -> pp_liberal_base ? 1 : 0, out_f))
+														{
+															if (WriteKeyValuePairForUnsignedInt ("primer_num_return", prefs_p -> pp_num_return, out_f))
+																{
+																	if (WriteKeyValuePairForUnsignedInt ("primer_explain_flag", prefs_p -> pp_explain_flag ? 1 : 0, out_f))
+																		{
+																			if (WriteKeyValuePairForString ("primer_max_size", primer_config_path_s, out_f))
+																				{
+																					success_flag = true;
+																				}		/* if (WriteKeyValuePairForString ("primer_max_size", primer_config_path_s, out_f)) */
+
+																		}		/* if (WriteKeyValuePairForUnsignedInt ("primer_max_size", prefs_p -> pp_explain_flag ? 1 : 0, out_f)) */
+
+																}		/* if (WriteKeyValuePairForUnsignedInt ("primer_max_size", prefs_p -> pp_max_size, out_f)) */
+
+														}		/* if (WriteKeyValuePairForUnsignedInt ("primer_max_size", prefs_p -> pp_max_size, out_f)) */
+
+												}		/* if (WriteKeyValuePairForUnsignedInt ("primer_max_size", prefs_p -> pp_max_size, out_f)) */
+
+										}		/* if (WriteKeyValuePairForUnsignedInt ("primer_max_size", prefs_p -> pp_max_size, out_f)) */
+
+								}		/* if (WriteKeyValuePairForString ("primer_product_size_range", range_s, out_f)) */
+
+							FreeCopiedString (range_s);
+						}		/* if (range_s) */
+
+
+					if (fclose (out_f) != 0)
+						{
+							PrintErrors (STM_LEVEL_SEVERE, __FILE__, __LINE__, "Failed to close primer config file \"%s\"", filename_s);
+						}
+
+				}		/* if (out_f) */
+
+			FreeCopiedString (filename_s);
 		}		/* if (filename_s) */
 
 	return success_flag;
@@ -122,13 +157,17 @@ bool GetPrimer3PrefsParameterTypesForNamedParameters (struct Service *service_p,
 		{
 			*pt_p = S_PROD_SIZE_MAX.npt_type;
 		}
-	else if (strcmp (param_name_s, S_LIBERAL_BASE.npt_name_s) == 0)
+	else if (strcmp (param_name_s, S_MAX_SIZE.npt_name_s) == 0)
 		{
-			*pt_p = S_LIBERAL_BASE.npt_type;
+			*pt_p = S_MAX_SIZE.npt_type;
 		}
 	else if (strcmp (param_name_s, S_LIB_AMBIGUITY_CODES_CONSENSUS.npt_name_s) == 0)
 		{
 			*pt_p = S_LIB_AMBIGUITY_CODES_CONSENSUS.npt_type;
+		}
+	else if (strcmp (param_name_s, S_LIBERAL_BASE.npt_name_s) == 0)
+		{
+			*pt_p = S_LIBERAL_BASE.npt_type;
 		}
 	else if (strcmp (param_name_s, S_PP_NUM_RETURN.npt_name_s) == 0)
 		{
@@ -150,6 +189,76 @@ bool GetPrimer3PrefsParameterTypesForNamedParameters (struct Service *service_p,
 bool ParsePrimer3PrefsParameters (ParameterSet *params_p, Primer3Prefs *prefs_p)
 {
 	bool success_flag = false;
+
+	return success_flag;
+}
+
+
+static char *GetProductSizeRangeAsString (const uint32 min_value, const uint32 max_value)
+{
+	char *range_s = NULL;
+	char *min_s = GetUnsignedIntAsString (min_value);
+
+	if (min_s)
+		{
+			char *max_s = GetUnsignedIntAsString (max_value);
+
+			if (max_s)
+				{
+					range_s = ConcatenateVarargsStrings (min_s, "-", max_s, NULL);
+
+					if (!range_s)
+						{
+							PrintErrors (STM_LEVEL_SEVERE, __FILE__, __LINE__, "Failed to concatenate \"%s\", \"-\" and \"%s\"", min_s, max_s);
+						}
+
+					FreeCopiedString (max_s);
+				}		/* if (max_s) */
+			else
+				{
+					PrintErrors (STM_LEVEL_SEVERE, __FILE__, __LINE__, "Failed to get " UINT32_FMT " as a string", max_value);
+				}
+
+			FreeCopiedString (min_s);
+		}		/* if (min_s) */
+	else
+		{
+			PrintErrors (STM_LEVEL_SEVERE, __FILE__, __LINE__, "Failed to get " UINT32_FMT " as a string", min_value);
+		}
+
+	return range_s;
+}
+
+
+static bool WriteKeyValuePairForString (const char *key_s, const char *value_s, FILE *out_f)
+{
+	bool success_flag = false;
+
+	if (fprintf (out_f, "%s=%s\n", key_s, value_s) >= 0)
+		{
+			success_flag = true;
+		}
+	else
+		{
+			PrintErrors (STM_LEVEL_SEVERE, __FILE__, __LINE__, "Failed to write \"%s\"=\"%s\"", key_s, value_s);
+		}
+
+	return success_flag;
+}
+
+
+static bool WriteKeyValuePairForUnsignedInt (const char *key_s, const uint32 value, FILE *out_f)
+{
+	bool success_flag = false;
+
+	if (fprintf (out_f, "%s=" UINT32_FMT "\n", key_s, value) >= 0)
+		{
+			success_flag = true;
+		}
+	else
+		{
+			PrintErrors (STM_LEVEL_SEVERE, __FILE__, __LINE__, "Failed to write \"%s\"=" UINT32_FMT, key_s, value);
+		}
 
 	return success_flag;
 }
